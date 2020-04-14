8d4cda5cbb3257bffc140de0a9152378750f88e3
Shepa <kyklovod98@gmail.com>
2020-04-14 18:05:55 +0300

    [+] Added 5Lab & apk

---
+++ Assets/Scripts/Lab5Button.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Lab5Button : MonoBehaviour
+{
+    public Button thisButton;
+
+    private void Start()
+    {
+        thisButton.onClick.AddListener(DestroyObj);
+    }
+
+    public void DestroyObj()
+    {
+        Debug.Log("DESTROY");
+        // Удаление себя
+        Destroy(gameObject);
+    }
+
+}
---
+++ Assets/Scripts/Lab5Main.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Lab5Main : MonoBehaviour
+{
+    public Button addRecord;
+    public GameObject containerForObject;
+
+    private Button bttnListObj;
+    // Для удобства сделаем определенный максимум для обьектов, чтоб их нельзя было создать бесконечно много
+    private int countObj=0;
+
+    private void Start()
+    {
+        addRecord.onClick.AddListener(CreateNewObject);
+    }
+
+    private void CreateNewObject()
+    {
+        // Проверка на колличество обьектов
+        if (countObj < 25)
+        {
+            // Instantiate - Создание обьекта
+            // Resources.Load - Загрузка обьекта с ресурсов
+            // containerForObject.transform - Определение родительского обьекта, куда будет помещен обьект при создании
+            bttnListObj = Instantiate(Resources.Load<Button>("ListButton5Lab"), containerForObject.transform);
+            countObj++;
+            // Получаем компонент текста в дочерних обьектах для передачи названия
+            bttnListObj.gameObject.GetComponentInChildren<Text>().text = "Record " + countObj;
+        }
+        else
+        {
+            Debug.Log("Maximum count record reached!");
+        }
+    }
+}
