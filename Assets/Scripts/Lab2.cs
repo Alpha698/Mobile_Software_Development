11ae5b02ab5124a511e5bf099e0171cb0a6014e7
Shepa <tsef47@mail.ru>
2020-04-23 23:58:14 +0300

    [+] Added ukr reports [+] Added 6Lab & apk [*] Rebuild apk-s for 1, 2, 3, 4, 5 labs

--- Assets/Scripts/Lab2.cs
+++ Assets/Scripts/Lab2.cs
@@ -13,14 +13,12 @@ public class Lab2 : MonoBehaviour
     {
         if (Screen.orientation == ScreenOrientation.LandscapeLeft || Screen.orientation == ScreenOrientation.LandscapeRight)
         {
-            Debug.Log("Landscape");
             Title.text = "Горизонтальная ориентация экрана";
             VerticalGroup.SetActive(false);
             HorizontalGroup.SetActive(true);
         }
         else if (Screen.orientation == ScreenOrientation.Portrait)
         {
-            Debug.Log("Portrait");
             Title.text = "Вертикальная ориентация экрана";
             VerticalGroup.SetActive(true);
             HorizontalGroup.SetActive(false);
---
+++ Assets/Scripts/Lab6.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Lab6 : MonoBehaviour
+{
+    [Header ("Окна приложения")]
+    public GameObject mainPanel;
+    public GameObject settingsPanel;
+
+    [Header("Обьекты главного окна")]
+    public TextMeshProUGUI textNotification;
+    public TextMeshProUGUI textAdress;
+    public TextMeshProUGUI textAboutCheckBox2;
+    public Button openSettings;
+
+    [Header("Обьекты окна настроек")]
+    public Toggle flagNotification;
+    public GameObject adressPanel;
+    public InputField enterAdress;
+    public Toggle flagCheckBox2;
+    public Button saveSettings;
+    public Button clearSettings;
+
+    private void Start()
+    {
+        openSettings.onClick.AddListener(OpenSettings);
+        saveSettings.onClick.AddListener(SavePrefSettings);
+        clearSettings.onClick.AddListener(ClearPrefSettings);
+    }
+
+    private void Update()
+    {
+        //считывание данных с плеерпреф
+        textNotification.text = PlayerPrefs.GetString("Notification");
+        textAdress.text = PlayerPrefs.GetString("Adress");
+        textAboutCheckBox2.text = PlayerPrefs.GetString("CheckBox2");
+        //скрывать поле ввода адреса если чек бокс не выставлен
+        if (flagNotification.isOn)
+        {
+            adressPanel.SetActive(true);
+        }
+        else
+        {
+            adressPanel.SetActive(false);
+        }
+    }
+
+    private void OpenSettings()
+    {
+        mainPanel.SetActive(false);
+        settingsPanel.SetActive(true);
+        //считывание данных с плеерпреф
+        if (PlayerPrefs.GetString("Notification") == "True")
+        {
+            flagNotification.isOn = true;
+        }
+        else
+        {
+            flagNotification.isOn = false;
+        }
+        enterAdress.text = PlayerPrefs.GetString("Adress");
+        if (PlayerPrefs.GetString("CheckBox2") == "True")
+        {
+            flagCheckBox2.isOn = true;
+        }
+        else
+        {
+            flagCheckBox2.isOn = false;
+        }
+    }
+    private void SavePrefSettings()
+    {
+        //сохранение данных в кэш
+        PlayerPrefs.SetString("Notification", flagNotification.isOn.ToString());
+        PlayerPrefs.SetString("Adress", enterAdress.text);
+        PlayerPrefs.SetString("CheckBox2", flagCheckBox2.isOn.ToString());
+        //закрытие окна
+        mainPanel.SetActive(true);
+        settingsPanel.SetActive(false);
+    }
+    private void ClearPrefSettings()
+    {
+        //удаление данных из кэша
+        PlayerPrefs.DeleteAll();
+        //закрытие окна
+        mainPanel.SetActive(true);
+        settingsPanel.SetActive(false);
+    }
+}
